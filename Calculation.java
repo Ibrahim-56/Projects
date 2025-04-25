1c6d261031c9da1e242d4db40abb89d8e4feb14f
Ibrahim-56 <bfh33355@gmail.com>
2025-04-26 01:47:08 +0300

    test logic & some specific calculations

--- Calculation.java
+++ Calculation.java
@@ -1,43 +1,12 @@
-package test;
-import java.util.StringTokenizer;
-public class Calculation {
-
- public int findMax(int arr[]){
-
- int max=0;
- for(int i=1;i<arr.length;i++){
- if(max<arr[i])
- max=arr[i];
- }
- return max;
- }
-
-
-
- public int findMaxeEdit(int arr[]){
-
- int max=arr[0];
- for(int i=1;i<arr.length;i++){
- if(max<arr[i])
- max=arr[i];
- }
- return max;
- }
-
-
- public String reverseWord(String str){
-
- StringBuilder result=new StringBuilder();
- StringTokenizer tokenizer=new StringTokenizer(str," ");
-
- while(tokenizer.hasMoreTokens()){
- StringBuilder sb=new StringBuilder();
- sb.append(tokenizer.nextToken());
- sb.reverse();
-
- result.append(sb);
- result.append("");
- }
- return result.toString();
- }
-}
+package com.javatpoint.logic;
+public class Calculation {  
+	  
+    public static int findMax(int arr[]){  
+        int max=arr[0];//arr[0] instead of 0  
+        for(int i=1;i<arr.length;i++){  
+            if(max<arr[i])  
+                max=arr[i];  
+        }  
+        return max;  
+    }  
+}  
